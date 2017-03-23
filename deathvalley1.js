d5df9c2a954d336e03f83caa1524946a7cb10728
Dmytro <dmuktro@users.noreply.github.com>
2017-03-23 13:55:09 +0200

    Update deathvalley1.js

--- deathvalley1.js
+++ deathvalley1.js
@@ -11,11 +11,9 @@
 // for ( ; ; )
 
 var numSheep = 4;
-var monthNumber = 1;
 var monthsToPrint = 12;
-while ( monthNumber <= monthsToPrint ) {
-  numSheep= numSheep *4;
-  console.log('There will be ' + numSheep + ' sheep after ' + monthNumber + ' month(s)!');
-  monthNumber++; 
-}
 
+for (var monthNumber =1; monthNumber <= monthsToPrint; monthNumber++) {
+  numSheep = numSheep * 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+}
