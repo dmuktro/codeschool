fbc207ad24a302cb4127a854aa3f9934aea61707
Dmytro <dmuktro@users.noreply.github.com>
2017-03-23 14:50:36 +0200

    Update hooverDam.js

--- hooverDam.js
+++ hooverDam.js
@@ -15,12 +15,12 @@ var currentGen = 1;
 var totalGen = 19;
 var totalMW = 0;
 
-while ( currentGen <=4 ) {
+while ( currentGen <=4 ){
   totalMW = totalMW + 62;
   console.log('Generator #' + currentGen + ' is on, adding 62 MW, for a total of ' + totalMW + ' MW!');
-  currentGen= currentGen + 1;
+  currentGen++;
 }  
-for ( var currentGen =5; currentGen <= 19; currentGen++) {
+for ( var currentGen = 5; currentGen <= totalGen; currentGen++){
 	totalMW = totalMW + 124;
   console.log('Generator #' + CurrentGen + ' is on, adding 124 MW, for a total of ' + totalMW + ' MW!');
 }
