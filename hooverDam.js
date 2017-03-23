63a2caf09dcbe22bce699884ffc75b6f4a2c9097
Dmytro <dmuktro@users.noreply.github.com>
2017-03-23 13:59:10 +0200

    Update hooverDam.js

--- hooverDam.js
+++ hooverDam.js
@@ -1,7 +1,16 @@
 // http://javascript-roadtrip-part2.codeschool.com/levels/1/challenges/7
 
-// The Hoover Dam has 19 generators of multiple types. For simplicity, let’s say that the first 4 of these generators output 62 megawatts, and the other 15 output 124 megawatts. In hooverDam.js, the Dam Rangers have asked you to design a system of two loops that turns each generator on in progression, and prints the new total of megawatts generated.
-// They’d like the first loop to be a while loop handling the first 4 generators. Then, they’d like the second loop to be a for loop that handles the other 15 generators. Each output line should resemble the following lines, with adjusted values for the currentGen and totalMW:
+// The Hoover Dam has 19 generators of multiple types. For simplicity, let’s say that 
+// the first 4 of these generators output 62 megawatts, and 
+// the other 15 output 124 megawatts. In hooverDam.js, the Dam Rangers 
+// have asked you to design a system of two loops that turns 
+// each generator on in progression, and prints the new total of megawatts generated.
+// They’d like the first loop to be a while loop handling the first 4 generators. 
+// Then, they’d like the second loop to be a for loop that handles the other 15 generators. 
+// Each output line should resemble the following lines, with adjusted values for the currentGen and totalMW:
 //   Generator #1 is on, adding 62 MW, for a total of 62 MW!
 //   Generator #2 is on, adding 62 MW, for a total of 124 MW!
 
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
