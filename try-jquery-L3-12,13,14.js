db18ba5249bb981b2aaf01bf1ab2f85dd36cbcb2
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 01:10:49 +0300

    Update try-jquery-L3-12,13,14.js

--- try-jquery-L3-12,13,14.js
+++ try-jquery-L3-12,13,14.js
@@ -6,7 +6,10 @@
 //  ..we're no longer appending the message in the right place. We really only want this message to be added 
 //  after() the button we click on. Instead of appending the message to the .usa list, 
 //  add it after() the button that was clicked.  
-//   
+//   It looks like someone changed our HTML. Now the <button> is inside a <div> tag. 
+// We don't want the message to go inside the <div> tag though, we want 
+// it to go at the bottom of the <li> element. Instead of using after(), 
+// let's change our code to find the closest() .tour element and append() the message to the bottom of it.
 $(document).ready(function() {
   $('button').on('click', function() {
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
