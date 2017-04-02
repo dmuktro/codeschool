937fbc011e2a275ea156f5bd638cd9e17fc34c60
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 01:25:31 +0300

    Update and rename try-jquery-L3-12,13,14.js to try-jquery-L3-12,13,14,15.js

--- try-jquery-L3-12,13,14,15.js
+++ try-jquery-L3-12,13,14,15.js
@@ -1,6 +1,7 @@
 // http://try.jquery.com/levels/3/challenges/12 
 //  http://try.jquery.com/levels/3/challenges/13 
 //   http://try.jquery.com/levels/3/challenges/14
+//    http://try.jquery.com/levels/3/challenges/15
 // When we click on one of the buttons, it removes all buttons on the page. 
 // Instead, let's just remove the one button that was clicked by using this. 
 //  ..we're no longer appending the message in the right place. We really only want this message to be added 
@@ -10,6 +11,10 @@
 // We don't want the message to go inside the <div> tag though, we want 
 // it to go at the bottom of the <li> element. Instead of using after(), 
 // let's change our code to find the closest() .tour element and append() the message to the bottom of it.
+//    ..we've decided to allow travelers to click on the entire <li> element. 
+// Change the call to on() to target .tour elements instead of button elements. 
+// After that change, $(this) will reference the clicked <li>. Let's 
+// remove the closest() method so that .append(message) still works. Then, use find to locate the button element and remove() it.
 $(document).ready(function() {
   $('button').on('click', function() {
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
