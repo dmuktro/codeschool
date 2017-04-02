e02b303f776541172132ee7a6916a4ee3c5986c4
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 00:33:48 +0300

    Create try-jquery-L3-10.js

---
+++ try-jquery-L3-10.js
@@ -0,0 +1,8 @@
+// http://try.jquery.com/levels/3/challenges/12
+$(document).ready(function() {
+  $('button').on('click', function() {
+    var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
+    $('.usa').append(message);
+    $('button').remove();
+  });
+});
