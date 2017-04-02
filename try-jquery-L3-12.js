ca43bfb505d23978ccb279c46b930ad59021f0d3
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 00:35:37 +0300

    Update try-jquery-L3-12.js

--- try-jquery-L3-12.js
+++ try-jquery-L3-12.js
@@ -1,4 +1,6 @@
-// http://try.jquery.com/levels/3/challenges/12
+// http://try.jquery.com/levels/3/challenges/12 
+// When we click on one of the buttons, it removes all buttons on the page. 
+//  Instead, let's just remove the one button that was clicked by using this. 
 $(document).ready(function() {
   $('button').on('click', function() {
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
