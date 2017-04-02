2f48df836dd8a2a3d50e021f65f3333bb1329baa
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 00:36:21 +0300

    Update try-jquery-L3-12.js

--- try-jquery-L3-12.js
+++ try-jquery-L3-12.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   $('button').on('click', function() {
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
     $('.usa').append(message);
-    $('button').remove();
+    $(this).remove();
   });
 });
+
