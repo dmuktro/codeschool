ecd55df5309ee7b0b2b89e2b2c3e55b84b361502
Dmytro <dmuktro@users.noreply.github.com>
2017-04-03 19:35:10 +0300

    Create try-jQuery-L3-20.js

---
+++ try-jQuery-L3-20.js
@@ -0,0 +1,14 @@
+// <!-- usage of jQuery as in  http://try.jquery.com/levels/3/challenges/20  --> 
+//  There is a small problem with the way our on() handler is being used. 
+//  Currently, it is targeting all of the <button> elements on the screen. Refactor the 
+//  on() handler to only target <button> elements within a 
+//  .tour by using the selector argument of the on() method.
+//    some result filtering options to our page. We want to be able to click on a filter and 
+//    highlight the corresponding tours with a .highlight class. Go ahead and get started by 
+//    creating the click handler for the .on-sale filter from the #filters list. 
+//    Don't worry about the contents of the click handler function, 
+//    we'll get it working in the next few challenges.
+
+$(document).ready(function() {
+  //Create the click handler here
+  
