49b9b357c2af9bae2f957a7666717e6cd948cf7d
Dmytro <dmuktro@users.noreply.github.com>
2017-04-04 19:48:58 +0300

    Update try-jQuery-L4-4_cs.js

--- try-jQuery-L4-4_cs.js
+++ try-jQuery-L4-4_cs.js
@@ -2,9 +2,17 @@
 // After the DOM has loaded, let's add an event handler to watch for a click on button elements. 
 // Just to make sure this doesn't impact anything else on the page, 
 // only watch for clicks on buttons within the #tour element.
+// ..The photos will now be shown, but we have no way of hiding them. 
+// Let's change this to use slideToggle so that the photos will be hidden if they click again.
 
 $(document).ready(function() {
-  $("#tour").on("click",'button',someF);
-  function someF() {
-  }
+  $('#tour').on('click','button',function() {
+  	  alert('there are ' + $('img').length + ' images here');
+      $('.photos').slideDown('slow',function() {
+// Animation complete.
+  });
+      $('.photos').slideToggle() {
+// togglinG complete.
+  });
+  });
 });
