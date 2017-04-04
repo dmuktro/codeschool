f6ffdf10978ad30244973246b86303e6a85ca4de
Dmytro <dmuktro@users.noreply.github.com>
2017-04-04 17:26:30 +0300

    Update and rename try-jQuery-L3-23.js to try-jQuery-L3-23m.js

--- try-jQuery-L3-23m.js
+++ try-jQuery-L3-23m.js
@@ -11,15 +11,24 @@
 //    highlight class immediately after clicking a filter so we are only 
 //    highlighting the correct elements. Another filter has been added to make things interesting. 
 //    In both of these filters, remove the highlight class from anything that has it.
+// <!-- usage of jQuery as in  http://try.jquery.com/levels/3/challenges/24,  --> 
+//  some code of challenges #23 
 
 $(document).ready(function() {
   $('#filters').on('click', '.on-sale', function() {
     $('.highlight').removeClass('highlight');
     $('.tour').filter('.on-sale').addClass('highlight');
+    $('.clearing').text("!clear highlight");
   });
 
   $('#filters').on('click', '.featured', function() {
     $('.highlight').removeClass('highlight');
     $('.tour').filter('.featured').addClass('highlight');
+    $('.clearing').text("!clear highlight");
+  });
+
+  $('#filters').on('click', '.clearing', function() {
+    $('.highlight').removeClass('highlight');
+    $('.clearing').text("[reserved]");
   });
 });
