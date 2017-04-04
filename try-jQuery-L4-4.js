ccc6d3869edff5ac19cadbc9a58ffe945687763e
Dmytro <dmuktro@users.noreply.github.com>
2017-04-04 18:46:32 +0300

    Update try-jQuery-L4-4.js

--- try-jQuery-L4-4.js
+++ try-jQuery-L4-4.js
@@ -1,2 +1,10 @@
 // http://try.jquery.com/levels/4/challenges/4 (4.4)
-$("button").on("click");
+// After the DOM has loaded, let's add an event handler to watch for a click on button elements. 
+// Just to make sure this doesn't impact anything else on the page, 
+// only watch for clicks on buttons within the #tour element.
+
+$(document).ready(function() {
+  $("#tour").on("click",'button',someF);
+  function someF() {
+  }
+});
