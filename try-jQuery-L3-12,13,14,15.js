d5433042807e82f6f06bda91daacafd55c31c1f0
Dmytro <dmuktro@users.noreply.github.com>
2017-04-05 12:57:04 +0300

    Update and rename try-jquery-L3-12,13,14,15.js to try-jQuery-L3-12,13,14,15.js

--- try-jQuery-L3-12,13,14,15.js
+++ try-jQuery-L3-12,13,14,15.js
@@ -15,6 +15,7 @@
 // Change the call to on() to target .tour elements instead of button elements. 
 // After that change, $(this) will reference the clicked <li>. Let's 
 // remove the closest() method so that .append(message) still works. Then, use find to locate the button element and remove() it.
+
 $(document).ready(function() {
   $('li').on('click', function() {
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
